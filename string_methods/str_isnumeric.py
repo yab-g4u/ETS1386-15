e6350e0e098dc6c156fd7cf34b5e1ab2ccbd48a3
Betha <140443069+yab-g4u@users.noreply.github.com>
2025-03-20 19:07:02 +0200

    isnumeric method to check if a string contains all numbers or not

---
+++ string_methods/str_isnumeric.py
@@ -0,0 +1,5 @@
+num = "1,2,3,4,5,6,7" # consist of a string with numbers 
+
+is_numeric = num.isnumeric() # will check the variable num if it contains all numbers or not
+
+print(is_numeric) # we expect True to be printed
