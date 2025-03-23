4783174f9ddb43d101a4e4e58120f2cd9e7fa547
Betha <140443069+yab-g4u@users.noreply.github.com>
2025-03-23 17:18:23 +0200

    a string_method called str.count() to count a specified string

---
+++ string_methods/str_count.py
@@ -0,0 +1,8 @@
+ #using the string method str.count()
+
+text = "count how many A's are in this string A A A A"
+
+count_method = text.count("A") # we expect this method to count the A's in the variable text
+
+print(count_method)
+
