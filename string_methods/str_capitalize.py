b2af567c8588483d4096e86864f361bd5d941339
Betha <140443069+yab-g4u@users.noreply.github.com>
2025-03-23 17:16:51 +0200

    Create str_capitalize.py

---
+++ string_methods/str_capitalize.py
@@ -0,0 +1,7 @@
+# using the string method capitalize 
+
+test_sentence = "i want to be good at python to use it in machine learning world"
+
+capitalize_method = test_sentence.capitalize()
+
+print(capitalize_method)
