307102b98311705c2db4d63e14cfbec83e33c9ae
Betha <140443069+yab-g4u@users.noreply.github.com>
2025-03-25 17:35:09 +0200

    wrong file

--- str_capitalize.py
+++
@@ -1,7 +0,0 @@
-# using the string method capitalize 
-
-test_sentence = "i want to be good at python to use it in machine learning world"
-
-capitalize_method = test_sentence.capitalize()
-
-print(capitalize_method)
