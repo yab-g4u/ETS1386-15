c96ad289e6a259626d0fc267e8ec2933c475616a
Betha <140443069+yab-g4u@users.noreply.github.com>
2025-03-25 17:35:39 +0200

    wrong file

--- str_count.py
+++
@@ -1,8 +0,0 @@
- #using the string method str.count()
-
-text = "count how many A's are in this string A A A A"
-
-count_method = text.count("A") # we expect this method to count the A's in the variable text
-
-print(count_method)
-
