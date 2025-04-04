60bfb0ed60bbf2aee7bc2870d7cdb0fe6e88cdca
Betha <140443069+yab-g4u@users.noreply.github.com>
2025-04-04 22:45:21 +0300

    Create List_iteration.py

---
+++ List_iteration.py
@@ -0,0 +1,12 @@
+# List of colors
+colors = ['red', 'green', 'blue', 'yellow']
+
+# Iterating through the list and printing each color
+for color in colors:
+    print(color)
+
+# i expect the Output:
+# red
+# green
+# blue
+# yellow
