994dbb9309a1e3972c6b17b07b43d8cbe089589f
Betha <140443069+yab-g4u@users.noreply.github.com>
2025-04-04 22:49:20 +0300

    Appending elements to the list

---
+++ List_append.py
@@ -0,0 +1,9 @@
+# Creating an empty list
+fruits = []
+
+# Appending items to the list
+fruits.append("apple")
+fruits.append("banana")
+fruits.append("cherry")
+
+print(fruits)  # Output: ['apple', 'banana', 'cherry']
